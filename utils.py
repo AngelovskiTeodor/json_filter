08b47b888e7464eee30461d449ddcdcbcd2031c8
AngelovskiTeodor <angelovski.teo@outlook.com>
2022-06-24 12:59:54 +0200

    Added utility functions for filter and sorting options

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import datetime
+
 def setFilterOptions(args):
     minimum_rating = int(args[0])
     rating = True if args[1].upper()=="HIGHEST" else False
@@ -6,3 +8,20 @@ def setFilterOptions(args):
 
     return minimum_rating, rating, text, date
 
+def text_prioritize(prioritize, value):
+    if prioritize:
+        return not value
+    else:
+        return value
+
+def prioritize_highest_rating(prioritize, value):
+    if prioritize:
+        return -value
+    else:
+        return value
+    
+def prioritize_newest(prioritize, value):
+    if prioritize:
+        return datetime.utcnow()-value
+    else:
+        return value
