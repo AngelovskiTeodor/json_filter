14d95dcc04b0bb89288e50be50fe4d8d5bc2b601
AngelovskiTeodor <angelovski.teo@outlook.com>
2022-06-24 12:41:56 +0200

    Implemented reading filter options as CLI arguments

--- script.py
+++ script.py
@@ -1,7 +1,12 @@
 import json
+import sys
+from utils import setFilterOptions
 
 if __name__=="__main__":
     json_file = open('reviews.json', "r", encoding='utf-8-sig')
     json_data = json.load(json_file)
 
-    
+    args = sys.argv[1:]
+    if len(args)==4:
+        minimum_rating, rating, text, date = setFilterOptions(args)
+
