d55b2f61899f3f0f5104cf6004a246f97e535dbb
AngelovskiTeodor <angelovski.teo@outlook.com>
2022-06-24 12:43:48 +0200

    Added default values and CONSTS for filter options

--- script.py
+++ script.py
@@ -6,6 +6,15 @@ if __name__=="__main__":
     json_file = open('reviews.json', "r", encoding='utf-8-sig')
     json_data = json.load(json_file)
 
+    prioritize_by_text = True
+    HIGHEST_FIRST = True
+    LOWEST_FIRST = False
+    rating = HIGHEST_FIRST
+    NEWEST_FIRST = True
+    OLDEST_FIRST = False
+    date = OLDEST_FIRST
+    minimum_rating = 3
+
     args = sys.argv[1:]
     if len(args)==4:
         minimum_rating, rating, text, date = setFilterOptions(args)
